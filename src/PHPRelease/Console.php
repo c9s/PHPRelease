4fb92d07d436f1f936122b2df2fb2d1091ca9eb0
c9s <cornelius.howl@gmail.com>
2013-03-23 20:37:09 +0800

    Checking in changes prior to tagging of version 1.0.22.

--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -7,7 +7,7 @@ use PHPRelease\VersionReader;
 class Console extends Application
 {
     const NAME = "PHPRelease";
-    const VERSION = "1.0.21";
+    const VERSION = "1.0.22";
 
     public function brief()
     {
