962440ae095a4286d0cd4c00ff813c645cb8f639
c9s <cornelius.howl@gmail.com>
2013-03-23 23:24:34 +0800

    Checking in changes prior to tagging of version 1.0.30.

--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -156,9 +156,9 @@ class Console extends Application
             $steps = preg_split('#\s*,\s*#', $config['Steps'] );
         }
 
-        if ( $this->options->steps ) {
+        if ( $this->options->skip ) {
             $keys = array_combine( $steps , $steps );
-            foreach( $this->options->steps as $s ) {
+            foreach( $this->options->skip as $s ) {
                 unset($keys[$s]);
             }
             $steps = array_keys($keys);
