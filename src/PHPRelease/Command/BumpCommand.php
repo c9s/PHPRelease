440ad28155ad1d2af269ab38a45a8d2892d3e189
c9s <cornelius.howl@gmail.com>
2013-03-23 18:02:42 +0800

    runSteps

--- src/PHPRelease/Command/BumpCommand.php
+++ src/PHPRelease/Command/BumpCommand.php
@@ -5,5 +5,22 @@ use CLIFramework\Command;
 class BumpCommand extends Command
 {
     public function brief() { return "bump version"; }
+
+    public function options($opts) 
+    {
+        $opts->add('major','bump major version');
+        $opts->add('minor','bump minor version');
+        $opts->add('s|stability:','set stability');
+        foreach( array('dev','rc','beta','alpha','stable') as $s ) {
+            $opts->add($s, "set stability to $s.");
+        }
+    }
+
+    public function execute()
+    {
+        if ( file_exists('composer.json') ) {
+            // trying to read the version
+        }
+    }
 }
 
--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -1,10 +1,10 @@
 <?php
 namespace PHPRelease;
 use CLIFramework\Application;
+use RuntimeException;
 
 class Console extends Application
 {
-
     public function init()
     {
         parent::init();
@@ -13,11 +13,71 @@ class Console extends Application
         $this->registerCommand('release');
     }
 
-    /*
+    public function options($opts)
+    {
+        $opts->add('dry','dryrun mode.');
+    }
+
+    public function runSteps($steps, $dryrun = false)
+    {
+        foreach( $steps as $step ) {
+            if ( file_exists( $step ) ) {
+                if ( ! is_executable($step) ) {
+                    throw new RuntimeException("$step is not an executable.");
+                }
+                $this->logger->info("===> Running $step");
+                if ( ! $dryrun ) {
+                    $lastline = system($step, $retval);
+                    if ( $retval ) {
+                        $this->logger->error($lastline);
+                        $this->logger->error("===> $step failed, aborting...");
+                        exit(0);
+                    }
+                }
+            }
+
+            $task = null;
+            if ( class_exists( $step, true ) ) {
+                $task = new $step( $this->logger );
+            } else {
+                // built-in task
+                $taskClass = 'PHPRelease\\Tasks\\' . $step;
+                if ( class_exists($taskClass, true ) ) {
+                    $task = new $step( $this->logger );
+                }
+            }
+            if ( ! $task ) {
+                $this->logger->error("===> Taks $step not found, aborting...");
+                exit(0);
+            }
+
+            $this->logger->info("===> Running " . get_class($task) );
+            if ( ! $dryrun ) {
+                $retval = $task->run();
+                if ( false === $retval ) {
+                    $this->logger->error("===> $task failed, aborting...");
+                    exit(0);
+                }
+            }
+        }
+    }
+
     public function execute()
     {
-        $this->logger->info('here');
+        $config = parse_ini_file('.phprelease');
+        if ( isset($config['autoload']) ) {
+            if ( $a = $config['autoload'] ) {
+                $this->logger->info("===> Found autoload script, loading...");
+                $loader = require $a;
+            }
+        }
+        elseif ( file_exists('vendor/autoload.php') ) {
+            $this->logger->info("===> Found autoload script, loading...");
+            $loader = require "vendor/autoload.php";
+        }
+
+        $steps = preg_split('#\s*,\s*#',$config['steps'] );
+        $this->runSteps($steps, $this->options->dryrun);
     }
-    */
 }
 
