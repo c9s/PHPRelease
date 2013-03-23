5959b656751704b8576950ad2ced42451e291f53
c9s <cornelius.howl@gmail.com>
2013-03-23 20:17:32 +0800

    Checking in changes prior to tagging of version 1.0.11.

--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -7,7 +7,7 @@ use PHPRelease\VersionReader;
 class Console extends Application
 {
     const NAME = "PHPRelease";
-    const VERSION = "1.0.10";
+    const VERSION = "1.0.11";
 
     public $config = array();
 
