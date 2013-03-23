090d4048129fa0b97a07920b33c9aa1eb48a0040
c9s <cornelius.howl@gmail.com>
2013-03-23 16:34:58 +0800

    First commit

---
+++ scripts/phprelease.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+/**
+ * This file is part of the Onion package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+$app = PhpRelease\Console::getInstance();
+$app->run( $argv );
