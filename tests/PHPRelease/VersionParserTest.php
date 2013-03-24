cbf53b782b0d88c12e465245e2f4ee4a383eb150
c9s <cornelius.howl@gmail.com>
2013-03-24 09:11:43 +0800

    Refactor VersionParserTest

---
+++ tests/PHPRelease/VersionParserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class VersionParserTest extends PHPUnit_Framework_TestCase
+{
+    public function testBasicVersion()
+    {
+        $p = new PHPRelease\VersionParser;
+        $info = $p->parseVersionString('1.2.3');
+        ok($info);
+        is(1,$info['major']);
+        is(2,$info['minor']);
+        is(3,$info['patch']);
+        ok( ! $info['stability'] );
+    }
+
+    public function testVersionWithStability()
+    {
+        $p = new PHPRelease\VersionParser;
+        $info = $p->parseVersionString('1.2.3-rc');
+        ok($info);
+        is(1,$info['major']);
+        is(2,$info['minor']);
+        is(3,$info['patch']);
+        is( 'rc', $info['stability'] );
+    }
+}
+
