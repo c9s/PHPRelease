60267ac6a3a6fa6c69d9d3a60456be4fe8c84c9b
c9s <cornelius.howl@gmail.com>
2013-03-25 12:38:41 +0800

    Checking in changes prior to tagging of version 1.1.8.

--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -19,7 +19,7 @@ function findbin($bin)
 class Console extends Application
 {
     const NAME = "PHPRelease";
-    const VERSION = "1.1.7";
+    const VERSION = "1.1.8";
 
     public function brief()
     {
