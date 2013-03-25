465527c529bc14b12c3e59de820a17969ef30d02
c9s <cornelius.howl@gmail.com>
2013-03-25 23:07:01 +0800

    Check pacakge.ini

--- src/PHPRelease/Tasks/Onion.php
+++ src/PHPRelease/Tasks/Onion.php
@@ -5,8 +5,10 @@ class Onion extends BaseTask
 {
     public function execute()
     {
-        passthru("onion build", $retval);
-        return $retval == 0;
+        if ( file_exists('package.ini') ) {
+            passthru("onion build", $retval);
+            return $retval == 0;
+        }
     }
 }
 
