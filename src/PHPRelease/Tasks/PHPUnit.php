8322743bfc732473aff900d6fbea6ca6e1c203f1
c9s <yoanlin93@gmail.com>
2014-11-03 20:06:23 +0800

    PHPUnit config section

--- src/PHPRelease/Tasks/PHPUnit.php
+++ src/PHPRelease/Tasks/PHPUnit.php
@@ -7,7 +7,20 @@ class PHPUnit extends BaseTask
 
     public function execute()
     {
-        passthru("phpunit", $retval);
+        $command = array("phpunit");
+
+        if ($this->config('PHPUnit.Debug')) {
+            $command[] = '--debug';
+        }
+        if ($this->config('PHPUnit.Verbose')) {
+            $command[] = '--verbose';
+        }
+        if ($configFile = $this->config('PHPUnit.Configuration')) {
+            $command[] = '--configuration';
+            $command[] = $configFile;
+        }
+
+        passthru(join(' ', $command), $retval);
         return $retval == 0;
     }
 }
