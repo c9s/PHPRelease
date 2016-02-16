1fa5a099467917c35d84c245a4e2c3cf5d459b7c
c9s <yoanlin93@gmail.com>
2016-02-16 22:17:46 +0800

    Checking in changes prior to tagging of version 1.3.0.

--- src/PHPRelease/Console.php
+++ src/PHPRelease/Console.php
@@ -20,7 +20,7 @@ function findbin($bin)
 class Console extends Application
 {
     const NAME = "PHPRelease";
-    const VERSION = "1.2.0";
+    const VERSION = "1.3.0";
 
     public function brief()
     {
