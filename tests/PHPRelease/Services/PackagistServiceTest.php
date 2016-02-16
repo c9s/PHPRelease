b913663cf2c9ca0c01dc9b6c433b0e11ad11dce3
c9s <yoanlin93@gmail.com>
2016-02-16 22:43:40 +0800

    Add skipped message

--- tests/PHPRelease/Services/PackagistServiceTest.php
+++ tests/PHPRelease/Services/PackagistServiceTest.php
@@ -9,7 +9,7 @@ class PackagistServiceTest extends PHPUnit_Framework_TestCase
         $apiToken = getenv('PACKAGIST_APITOKEN');
 
         if (!$apiToken || !$usernmae) {
-            return $this->markTestSkipped();
+            return $this->markTestSkipped('PackagistServiceTest requires PACKAGIST_USERNAME and PACKAGIST_APITOKEN to be setup.');
         }
         $service = new PackagistService($username, $apiToken);
         $result = $service->updatePackage('https://github.com/c9s/PHPRelease');
